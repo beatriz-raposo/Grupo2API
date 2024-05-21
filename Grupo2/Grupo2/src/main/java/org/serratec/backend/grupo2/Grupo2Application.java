da68bbe6544bac9560828b3874aa1182258181d6
beatriz-raposo <162373882+beatriz-raposo@users.noreply.github.com>
2024-05-21 16:47:36 -0300

    Fix: editando o nome.

--- Grupo2/Grupo2/src/main/java/org/serratec/backend/grupo2/Grupo2Application.java
+++ Grupo2/Grupo2/src/main/java/org/serratec/backend/grupo2/Grupo2Application.java
@@ -1,4 +1,4 @@
-package org.serratec.backend.Grupo2;
+package org.serratec.backend.grupo2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
