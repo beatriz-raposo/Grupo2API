5f669aa2ef5367a5a80610d4d1bfa76c98273a43
beatriz-raposo <162373882+beatriz-raposo@users.noreply.github.com>
2024-05-21 16:35:32 -0300

    Feat: Criando projeto.

---
+++ Grupo2/Grupo2/src/test/java/org/serratec/backend/Grupo2/Grupo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serratec.backend.Grupo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
