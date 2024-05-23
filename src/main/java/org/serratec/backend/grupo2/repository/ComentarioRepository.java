8c43961794129535b1fa34ccb1eac74c744464e8
beatriz-raposo <162373882+beatriz-raposo@users.noreply.github.com>
2024-05-23 18:16:55 -0300

    fix: deletando @query.

--- src/main/java/org/serratec/backend/grupo2/repository/ComentarioRepository.java
+++ src/main/java/org/serratec/backend/grupo2/repository/ComentarioRepository.java
@@ -1,28 +1,11 @@
 package org.serratec.backend.grupo2.repository;
 
-import java.util.List;
-
 import org.serratec.backend.grupo2.model.Comentario;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ComentarioRepository extends JpaRepository<Comentario, Long> {
 
-//	// Consulta com paginação
-//	Page<Comentario> findByConteudoContaining(String texto, Pageable pageable);
-//
-//	// Consulta nativa
-//	@Query(value = "SELECT * FROM postagem p WHERE p.autor_id = ?1", nativeQuery = true)
-//	List<Comentario> findByAutorIdNative(Long autorId);
-//
-//	// Consulta JPQL
-//	@Query("SELECT p FROM Postagem p WHERE p.autor.id = :autorId")
-//	List<Comentario> findByAutorIdJPQL(Long autorId);
-//
-//	// Consulta Spring Data JPA
-//	List<Comentario> findByAutorId(Long autorId);
 }
+
