9a6db5c86c108c8e447901ce2489f91067d23c9b
beatriz-raposo <162373882+beatriz-raposo@users.noreply.github.com>
2024-05-25 03:15:24 -0300

    Fix: Ajustando codigo.

--- src/main/java/org/serratec/backend/grupo2/service/ComentarioService.java
+++ src/main/java/org/serratec/backend/grupo2/service/ComentarioService.java
@@ -3,6 +3,7 @@ package org.serratec.backend.grupo2.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.serratec.backend.grupo2.exception.NotFoundException;
 import org.serratec.backend.grupo2.model.Comentario;
 import org.serratec.backend.grupo2.repository.ComentarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +23,11 @@ public class ComentarioService {
 
     public Comentario buscar(@PathVariable Long id) {
         Optional<Comentario> comentarioOpt = comentarioRepository.findById(id);
+        if (comentarioOpt.isEmpty()) {
+        	throw new NotFoundException();
+        }
         return comentarioOpt.get();
-    }
+        }
 
     public Comentario inserir(Comentario comentario) {
         return comentarioRepository.save(comentario);
