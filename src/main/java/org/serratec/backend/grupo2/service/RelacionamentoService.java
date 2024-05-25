899eb7890058025929c82e9491be73c63bcf0298
beatriz-raposo <162373882+beatriz-raposo@users.noreply.github.com>
2024-05-24 21:10:34 -0300

    fix: Ajustando codigo.

--- src/main/java/org/serratec/backend/grupo2/service/RelacionamentoService.java
+++ src/main/java/org/serratec/backend/grupo2/service/RelacionamentoService.java
@@ -1,6 +1,6 @@
 package org.serratec.backend.grupo2.service;
 
-	import java.util.List;
+import java.util.List;
 import java.util.Optional;
 
 import org.serratec.backend.grupo2.model.Relacionamento;
@@ -9,28 +9,39 @@ import org.serratec.backend.grupo2.repository.RelacionamentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-	@Service
-	public class RelacionamentoService {
+@Service
+public class RelacionamentoService {
+	
+//	Private Relacionamento relacionamento;
 
-	    @Autowired
-	    private RelacionamentoRepository relacionamentoRepository;
+	@Autowired
+	private RelacionamentoRepository relacionamentoRepository;
 
-	    public List<Relacionamento> findAll() {
-	        return relacionamentoRepository.findAll();
-	    }
-
-	    public Optional<Relacionamento> findById(RelacionamentoPK id) {
-	        return relacionamentoRepository.findById(id);
-	    }
-
-	    public Relacionamento save(Relacionamento relacionamento) {
-	        return relacionamentoRepository.save(relacionamento);
-	    }
-
-	    public void deleteById(RelacionamentoPK id) {
-	        relacionamentoRepository.deleteById(id);
-	    }
+	public List<Relacionamento> findAll() {
+		return relacionamentoRepository.findAll();
 	}
 
+	public Optional<Relacionamento> findById(RelacionamentoPK id) {
+		return relacionamentoRepository.findById(id);
+	}
 
+	public Relacionamento save(Relacionamento relacionamento) {
+		return relacionamentoRepository.save(relacionamento);
+	}
 
+	public void deleteById(RelacionamentoPK id) {
+		relacionamentoRepository.deleteById(id);
+	}
+//
+//	public Relacionamento inserir(RelacionamentoPK relacionamentoPK) {
+//	    	Usuario usuario = new Usuario();
+//	    	usuario.setSeguidores(null);
+//	}
+
+//	public adicionarRelacionamento(Long seguidor, Long seguindo) {
+//		
+//		RelacionamentoPK pk = new RelacionamentoPK(seguidor, seguindo)
+//		Relacionamento relacionamento = new Relacionamento(pk)	
+//	}
+
+}
