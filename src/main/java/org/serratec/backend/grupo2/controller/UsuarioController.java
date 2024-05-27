aa4a7b181931af1a98dc21379920ef5ad49e2446
Luiz1210 <gusazevg@gmail.com>
2024-05-26 21:46:23 -0300

    Feet: Subindo armazenamento de foto de perfil

--- src/main/java/org/serratec/backend/grupo2/controller/UsuarioController.java
+++ src/main/java/org/serratec/backend/grupo2/controller/UsuarioController.java
@@ -18,9 +18,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import io.jsonwebtoken.io.IOException;
 import jakarta.validation.Valid;
 
 @RestController
@@ -70,5 +73,10 @@ public class UsuarioController {
 		return ResponseEntity.ok(usuario);
 	}
 	
+    @PostMapping("/{id}/foto")
+    public ResponseEntity<Void> uploadProfilePicture(@PathVariable Long id, @RequestParam("file") MultipartFile file) throws IOException, NotFoundException, java.io.IOException {
+        usuarioService.uploadProfilePicture(file, id);
+        return ResponseEntity.noContent().build();
+    }
 	
 }
